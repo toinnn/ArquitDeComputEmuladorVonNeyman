9f18400455625618ed9845e40cc5a8912c3e2bbc
toinnn <32816292+toinnn@users.noreply.github.com>
2019-03-15 16:24:15 -0300

    1.03
    tem quase td e quase td ta incompleto

--- EmuladorVonNeyman.py
+++ EmuladorVonNeyman.py
@@ -7,35 +7,80 @@ def HexToDec ():
 class Registrador :
     def __init__(self):
         print("Ainda nao faço nada")
+        self.Memoria=bytes
 class CPU :
-    def __init__(self):
+    def __init__(self,Barramento):
         print("Ainda nao faço nada")
+        self.Barramento=Barramento
+        self.Decoder={"mov".encode("utf-8"):lambda E,i:self.mov(E,i),"add".encode("utf-8"):lambda x,y :self.add(x,y),"inc".encode("utf-8"):lambda x:self.inc(x),"imul".encode("utf-8"):lambda x,y,z : self.imul(x,y,z)}
     A=B=C=D=Pi=Registrador
+    def leituraRAM(self):
+        self.Barramento.SolicitaçãoRamDaCPU()
+    def exec (self) :
+        print("nao faço nada")
+    def mov (self,E,i) :
+        print("nao faço nada")
+    def add(self,x,y):
+        print("nao faço nada")
+    def inc(self,x):
+        print("nao faço nada")
+    def imul(self,x,y,z):
+        print("não faço nada")
 
 class RAM :
-    def __init__(self):
+    def __init__(self,Barramento):#Falta validar o tamnho dos dados que circulam na RAM
         print("Ainda nao faço nada")
+        self.Barramento=Barramento
+        self.Memoria=bytearray(128)
+        self.MemoriaAlocada=0
+    def leituraBarramento(self):
+        self.Memoria[self.MemoriaAlocada:self.MemoriaAlocada+len(self.Barramento.escritaRam())]=self.Barramento.escritaRam()
+        self.MemoriaAlocada+=len(self.Barramento.escritaRam())
+    def escritaBarramento(self):
+        if self.MemoriaAlocada >=32 :
+            self.Barramento.leituraRAM(self.Memoria[0:33])
+            self.MemoriaAlocada-=32
+        else :
+            self.Barramento.leituraRAM(self.Memoria[0:33])
+            self.MemoriaAlocada -=len(self.MemoriaAlocada)
+
+
 class Barramento :
     def __init__(self):
         print("Ainda nao faço nada")
-        self.CPU =[None]*43
-        self.ES=bytearray(43)
-        self.RAM=bytearray(43)
+        self.CPU =bytearray(32)
+        self.ES=bytearray(32)
+        self.RAM=bytearray(32)
         self.ESpreenchido=0
         self.CPUpreenchido = 0
         self.RAMpreenchido = 0
-
+        self.CPUfísica
+        self.RAMfísica
+        self.ESfísica
+        self.auxClockRam=0
+    def link (self,CPU,RAM,ES ):
+        self.CPUfísica=CPU
+        self.RAMfísica=RAM
+        self.ESfísica=ES
     def leituraES (self,dado) :
         print("nao faço nada")
         for n in range(self.ESpreenchido,self.ESpreenchido+len(dado)) :
-            
+            self.RAM[n]=dado[0]
+            dado.pop(0)
+            self.ESpreenchido+=1
+    def leituraRAM (self) :
+        print("Ola mundo")
+    def SolicitaçãoRamDaCPU(self) :
+        print("oi eu sou o mundo")
+        self.RAMfísica.
 
-
-
-    def leituraRam (self,dado) :
-
-
-
+    def escritaRam (self) :
+        if self.RAMpreenchido>=32 :
+            self.RAMpreenchido-=32
+            return  self.RAM[0:33]
+        else:
+            self.RAMpreenchido-=self.RAMpreenchido
+            return self.RAM[0:self.RAMpreenchido]
 
 class ES :
     def __init__(self,barramento):
@@ -45,7 +90,12 @@ class ES :
 
     def escrita (self,Barramento) :
         print("Ainda nao faço nada")
-        self.preenchido=0
+        if self.preenchido>32 :
+            self.preenchido-=32
+            self.Barramento.leituraES(self.buffer[0:33])
+        else:
+            self.Barramento.leituraES(self.buffer[0:len(self.preenchido())])
+            self.preenchido = 0
 
 
     def aloca (self,num) :
@@ -63,10 +113,17 @@ class ES :
                 return
     def Encoder(self,codigo) :
         if codigo[1] =="mov" :
-            self.aloca(bytearray(codigo))
+            comando=bytearray([1])
+            comando.extend(int(codigo[2]),int(codigo[3]))
+            self.aloca(comando)
+        elif codigo[1]=="add" :
+            comando = bytearray([2])
+            comando.extend()
 
     def Parser (self,codigo):
         input_string= "mov A, 2"
-        ex = r"(mov)\s+([\daAbBcCdDeEfF]*),\s+(\d+)"
+        ex = r"(mov)\s+0x([\d]*),\s+(\d+)"
         m=re.match(ex,codigo)
+        add=re.match(r"(add)\s+0x(\d+),\s+|",codigo)
         if m :
+            self.Encoder(codigo)
--- TesteMaroto.py
+++ TesteMaroto.py
@@ -1,4 +1,5 @@
 import re
+import bitstream
 from IPython import embed
 """
 input_string= "mov A, 2"
@@ -7,8 +8,18 @@ expression = r"mov\s+\w+,\s+\d+"
 m=re.match(ex,"mov A23eF, 23")
 print(m[2])
 """
-vetor=bytearray(2)
-
-inteiro =2
-inteiro+=1
-print(inteiro)
+ex =r"(mov)\s+0x([\d]*),\s+(\d+)"
+m=re.match(ex,"mov 0x03, 30")
+m[1].encode('utf-8')
+li=[m[1].encode('utf-8')]
+n=bytearray([2])
+n.extend([int(m[2]),int(m[3])])
+print(n)
+"""
+def chama (x,y) :
+    print("OLA")
+    print(x+y)
+dicionario={"mov":lambda x,y :chama(x,y)}
+dicionario["mov"](1,2)
+print(bytearray("mov"))
+"""
